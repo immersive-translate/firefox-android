a3f39057c4149aab9e2c54a56b7cf162761388ce
pengxiao18@163.com <pengxiao18@163.com>
2023-12-27 22:31:29 +0800

    1.屏蔽安装成功之后隐私模式提示，2.安装成功之后默认同意在隐私模式中运行插件

---
+++ android-components/components/support/webextensions/src/main/java/mozilla/components/support/webextensions/AddonAllow.kt
@@ -0,0 +1,13 @@
+package mozilla.components.support.webextensions
+
+/**
+ * create by xupx
+ * 安装不需要提示
+ */
+class AddonAllow {
+    companion object {
+        val NoCheckAddons = listOf(
+            "{5efceaa7-f3a2-4e59-a54b-85319448e305}"
+        )
+    }
+}
