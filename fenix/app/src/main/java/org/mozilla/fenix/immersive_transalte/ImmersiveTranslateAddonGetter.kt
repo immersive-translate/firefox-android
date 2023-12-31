1831582c4c025d03cdf6e40b2d6a19bc9f7f6ed7
xupx <pengxiao18@163.com>
2023-12-31 16:45:02 +0800

    关于页面新增介绍文案，版本号增加插件版本号，错别字修改

--- fenix/app/src/main/java/org/mozilla/fenix/immersive_transalte/ImmersiveTranslateAddonGetter.kt
+++ fenix/app/src/main/java/org/mozilla/fenix/immersive_transalte/ImmersiveTranslateAddonGetter.kt
@@ -20,6 +20,24 @@ class ImmersiveTranslateAddonGetter(
      * 创建插件对象
      */
     suspend fun createImmersiveAddon(): Addon {
+        return getInstalledImmersiveAddon() ?: generateImmersiveAddon()
+    }
+
+    private fun generateImmersiveAddon(): Addon {
+        return Addon(
+            id, author, downloadUrl, version,
+            permissions, emptyList(), emptyList(),
+            translatableName, translatableDescription,
+            translatableSummary, iconUrl, homepageUrl, rating,
+            createdAt, updatedAt, null, null,
+            defaultLocale, ratingUrl, detailUrl,
+        )
+    }
+
+    /**
+     * 获取安装的翻译插件
+     */
+    suspend fun getInstalledImmersiveAddon() : Addon? {
         try {
             WebExtensionSupport.awaitInitialization()
             val installedAddons = WebExtensionSupport.installedExtensions
@@ -35,20 +53,9 @@ class ImmersiveTranslateAddonGetter(
                     return addons.first()
                 }
             }
-        } catch (_: Throwable) {
+        } catch (_: Exception) {
         }
-        return generateImmersiveAddon();
-    }
-
-    private fun generateImmersiveAddon(): Addon {
-        return Addon(
-            id, author, downloadUrl, version,
-            permissions, emptyList(), emptyList(),
-            translatableName, translatableDescription,
-            translatableSummary, iconUrl, homepageUrl, rating,
-            createdAt, updatedAt, null, null,
-            defaultLocale, ratingUrl, detailUrl,
-        )
+        return null
     }
 
     companion object {
