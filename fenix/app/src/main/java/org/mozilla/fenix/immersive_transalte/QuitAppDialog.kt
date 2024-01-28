d41a908e0f4247e08aab1b10b2401b006267f846
xupx <pengxiao18@163.com>
2024-01-28 17:06:09 +0800

    feat: 首次打开，用户隐私协议交互优化

--- fenix/app/src/main/java/org/mozilla/fenix/immersive_transalte/QuitAppDialog.kt
+++ fenix/app/src/main/java/org/mozilla/fenix/immersive_transalte/QuitAppDialog.kt
@@ -5,12 +5,13 @@
 
 package org.mozilla.fenix.immersive_transalte
 
-import android.content.Context
+import android.app.Activity
 import android.graphics.drawable.ColorDrawable
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.PopupWindow
+import androidx.core.view.WindowCompat
 import org.mozilla.fenix.R
 import org.mozilla.fenix.databinding.DialogQuitAppLayoutBinding
 
@@ -19,21 +20,26 @@ import org.mozilla.fenix.databinding.DialogQuitAppLayoutBinding
  * created by xupx
  * on 2024-01-27
  */
-class QuitAppDialog(context: Context,
+class QuitAppDialog(context: Activity,
                     onQuitApp: () -> Unit,
                     onKnown: () -> Unit
 ) : PopupWindow(context) {
 
+    private val activity = context
     private var binding: DialogQuitAppLayoutBinding
 
     init {
         animationStyle = R.style.popup_window_anim
         binding = DialogQuitAppLayoutBinding.inflate(LayoutInflater.from(context))
         contentView = binding.root
-        width = context.resources.displayMetrics.widthPixels
-        height = context.resources.displayMetrics.heightPixels
-        isFocusable = true
+
+        val displaySize = DeviceUtil.getDeviceRealSize(context)
+        width = displaySize.width
+        height = displaySize.height
+
+        isFocusable = false
         isOutsideTouchable = false
+        isClippingEnabled = false
         setBackgroundDrawable(ColorDrawable(0x6F000000))
 
         binding.btnQuitApp.setOnClickListener {
@@ -48,6 +54,14 @@ class QuitAppDialog(context: Context,
 
     fun show(parent: View) {
         showAtLocation(parent, Gravity.BOTTOM, 0, 0)
+        setStatusBarTheme(false)
+    }
+
+    private fun setStatusBarTheme(isLight: Boolean) {
+        WindowCompat.getInsetsController(
+            activity.window,
+            activity.window.decorView
+        ).isAppearanceLightStatusBars = isLight
     }
 
 }
